00fee17a1f029982397d63bc009b9a0070566b85
Ebtisam Jelani <93232997+Ebtisam0402@users.noreply.github.com>
2025-10-01 21:34:01 -0700

    created array list of string with an empty list. then added two strings

--- src/StringPractice.java
+++ src/StringPractice.java
@@ -26,6 +26,8 @@ public class StringPractice {
     // Create an ArrayList of Strings and assign it to a variable
     ArrayList<String> colors = new ArrayList<>();
     // Add multiple strings to the List (OK to do one-by-one)
+    colors.add ("Red");
+     colors.add ("Brown");
 
     // Join all of the strings in the list together into a single string separated by commas
     // Use a built-in method to achieve this instead of using a loop
